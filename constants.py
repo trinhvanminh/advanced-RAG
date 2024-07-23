ff5997c18222e44470df1883cbf086ba591d37e9
Minh Trinh <minh.trinh@logixtek.com>
2024-07-23 15:24:39 +0700

    add openai

--- constants.py
+++ constants.py
@@ -1,16 +1,18 @@
 from langchain_google_genai import GoogleGenerativeAI
 from langchain_groq import ChatGroq
 from langchain_cohere import ChatCohere
+from langchain_openai import ChatOpenAI
 
 llm_map = {
     "gemini": GoogleGenerativeAI(model='gemini-1.5-pro', temperature=0),
     "cohere": ChatCohere(model="command-r", temperature=0),
-    "groq": ChatGroq(temperature=0, model_name="llama3-70b-8192")
+    "groq": ChatGroq(model_name="llama3-70b-8192", temperature=0),
+    "chatgpt": ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 }
-
 
 llm_label_map = {
     "gemini": 'Gemini (1.5-pro)',
     "cohere": 'Cohere (command-r)',
     "groq": 'Groq (llama3-70b-8192)',
+    "chatgpt": "OpenAI (gpt-3.5-turbo)",
 }
